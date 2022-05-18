d857ede6061fb2a0769cd1e56640a7a88392174c
ZobairSoodi <soodi.zobair.solicode@gmail.com>
2022-05-18 12:28:38 +0200

    ok

--- src/ToDoList/ToDoList.js
+++ src/ToDoList/ToDoList.js
@@ -3,10 +3,10 @@ import ToDoItem from '../ToDoItem/ToDoItem.js';
 
 
 function ToDoList(props) {
-    const [myList, setMyList] = useState(props.myList);
+    
     return (
         <ul>
-            {myList.map((item) => <ToDoItem key={item.id} title={item.title} status={item.status} />)}
+            {props.myList.map((item) => <ToDoItem key={item.id} title={item.title} status={item.status} />)}
         </ul>
     )
 }
--- src/index.js
+++ src/index.js
@@ -3,30 +3,51 @@ import ReactDOM from 'react-dom/client';
 import ToDoInput from './ToDoInput/ToDoInput.js';
 import ToDoItem from './ToDoItem/ToDoItem.js';
 import ToDoList from './ToDoList/ToDoList.js';
+import { useState } from 'react';
 
-if (localStorage.getItem("list") == null) {
-  localStorage.setItem("list", "[]");
-}
-let temp_list = JSON.parse(localStorage.getItem("list"));
 
-function add_to_list(e) {
+function Main(props) {
 
-  var inp2 = e.target;
-  var inp1 = inp2.previousSibling;
-  temp_list.push(
-    {
-      "id": temp_list.length+1,
-      "title": inp1.value,
+  if (localStorage.getItem("list") == null) {
+    localStorage.setItem("list", "[]");
+  }
+  let temp_list = JSON.parse(localStorage.getItem("list"));
+  const [myList, setMyList] = useState(temp_list);
+  //const [count, setCount] = useState(0);
+
+  const add_to_list = (e) => {
+
+    var inp2 = e.target;
+    var inp1 = inp2.previousSibling.value;
+    temp_list.push(
+      {
+        "id": temp_list.length + 1,
+        "title": inp1,
+        "status": ""
+      }
+    );
+    localStorage.setItem("list", JSON.stringify(temp_list));
+    
+    var obj = {
+      "id": temp_list.length + 1,
+      "title": inp1,
       "status": ""
-    }
+    };
+    setMyList([...myList, obj]);
+    //console.log(temp_list);
+    // setMyList([{'id': 2, 'name': 'test', 'status':'done'}]);
+    //setMyList([temp_list]);
+  }
+
+  return (
+    <>
+      <ToDoInput func={add_to_list} />
+      <ToDoList myList={myList}></ToDoList>
+    </>
   )
-  localStorage.setItem("list", JSON.stringify(temp_list));
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <>
-    <ToDoInput func={add_to_list} />
-    <ToDoList myList={temp_list}></ToDoList>
-  </>
+  <Main />
 )
